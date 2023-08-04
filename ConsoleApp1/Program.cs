586dd424bf2552d77064f9cce1d05bcc8dec0c43
Teruteru <teruterubouzu1024@gmail.com>
2023-08-04 21:27:36 +0900

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,31 +13,53 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        public static string BytesToStr(byte[] bytes)
+        {
+            StringBuilder str = new StringBuilder(bytes.Length * 2);
+
+            for (int i = 0; i < bytes.Length; i++)
+                str.AppendFormat("{0:X2}", bytes[i]);
+
+            return str.ToString();
+        }
+
         static void Main(string[] args)
         {
-            var randomByte = new byte[8192];
-            for (long i = 8; i < 256; i++)
+            if (args.Length == 0)
             {
-                var filename = String.Format("D:\\keys\\private\\privateKeys{0}.bin", i);
-                using (FileStream dest = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite))
+                return;
+            }
+            byte[] b = new byte[65 * 16777216];
+            using (FileStream stream = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+            using (BinaryReader r = new BinaryReader(stream))
+            {
+                r.Read(b, 0, b.Length);
+            }
+            using (SHA512 sha512 = SHA512.Create())
+            using (RIPEMD160 ripemd160 = RIPEMD160.Create())
+            using (RandomNumberGenerator generator = RandomNumberGenerator.Create())
+            {
+                bool con = true;
+                byte[] buf = new byte[3];
+                do
                 {
-                    dest.Seek(0x01ffffff, SeekOrigin.Begin);
-                    long needs = 536870912L;
-                    long al = 0x01ffffffL;
-                    needs -= al;
-                    int a = 0;
-                    using (var rng = new RNGCryptoServiceProvider())
+                    generator.GetBytes(buf);
+                    int index = ((buf[0] & 0xff) << 16) + ((buf[1] & 0xff) << 8) + ((buf[2] & 0xff));
+                    int offset = (index << 6) + index;
+                    for (int i = 0; i < 1090519040; i += 65)
                     {
-                        while (needs > 0)
+                        sha512.TransformBlock(b, offset, 65, null, 0);
+                        sha512.TransformFinalBlock(b, i, 65);
+                        if ((BinaryPrimitives.ReadUInt64BigEndian(ripemd160.ComputeHash(sha512.Hash)) & 0xffffffffffff0000L) == 0)
                         {
-                            a = Math.Min((int)needs, 8192);
-                            rng.GetBytes(randomByte, 0, a);
-                            dest.Write(randomByte, 0, a);
-                            needs -= a;
+                            Console.WriteLine("{0},{1}", index, i / 65);
+                            con = false;
                         }
+                        sha512.Initialize();
+                        ripemd160.Initialize();
                     }
-                }
-                Console.WriteLine(String.Format("{0} done.", filename));
+                    Console.Error.WriteLine("{0} done", index);
+                } while (con);
             }
         }
     }
